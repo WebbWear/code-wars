d1143392f202304db09b5be679df557626db8b05
Jeff Webb <webbwear@icloud.com>
2019-01-02 18:05:28 -0500

    tip calculator

---
+++ public/Katas/8 kyu Tip Calculator.js
@@ -0,0 +1,30 @@
+
+// Directions
+// Complete the function, which calculates how much you need to tip based on the total amount of the bill and the service.
+
+// You need to consider the following ratings:
+
+// Terrible: tip 0%
+// Poor: tip 5%
+// Good: tip 10%
+// Great: tip 15%
+// Excellent: tip 20%
+// The rating is case insensitive (so "great" = "GREAT"). If an unrecognised rating is received, then you need to return:
+
+
+
+
+function calculateTip(amount, rating) {
+    let tip;
+        if(rating.toLowerCase() == 'Terrible'.toLowerCase())tip = 0;
+          else if(rating.toLowerCase() == 'Poor'.toLowerCase())tip = 5;
+          else if(rating.toLowerCase() == 'Good'.toLowerCase())tip = 10;
+          else if(rating.toLowerCase() == 'Great'.toLowerCase())tip = 15;
+          else if(rating.toLowerCase() == 'Excellent'.toLowerCase())tip = 20;
+          else return "Rating not recognised"
+    
+    
+    
+    
+      return Math.ceil((amount*tip)/100);
+    }
