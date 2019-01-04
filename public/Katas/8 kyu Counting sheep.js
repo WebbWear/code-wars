8eea8d8e2c907797de4f7617c730ff001fd06095
Jeff Webb <webbwear@icloud.com>
2019-01-03 23:58:11 -0500

    +1

--- public/Katas/8 kyu Counting sheep.js
+++ public/Katas/8 kyu Counting sheep.js
@@ -1,6 +1,6 @@
 // Consider an array of sheep where some sheep may be missing from their place. 
 // We need a function that counts the number of sheep present in the array (true means present).
-
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
 
 function countSheeps (arrayOfSheep) {
     var sheepCounter = 0;
